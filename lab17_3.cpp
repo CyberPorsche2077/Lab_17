c1ecfffb9414c0f048d6c235f6326a778dc35b8f
CyberPorsche2077 <152591269+CyberPorsche2077@users.noreply.github.com>
2024-03-08 12:49:55 +0700

    Update lab17_3.cpp

--- lab17_3.cpp
+++ lab17_3.cpp
@@ -4,24 +4,6 @@
 #include<vector>
 using namespace std;
 
-vector<int> randomVector(int);
-void showVector(vector<int>);
-int dotProduct(vector<int>,vector<int>);
-
-int main(){
-	srand(time(0));
-	vector<int> x = randomVector(5);
-	vector<int> y = randomVector(5);
-	int z = dotProduct(x,y);
-	showVector(x);
-	cout << " . ";
-	showVector(y);
-	cout << " = " << z;
-	
-	return 0;
-}
-
-//Write Definition of randomVector() showVector() and dotProduct() here
 vector<int> randomVector(unsigned int N){	
     vector<int> v;
     for(unsigned int i = 0; i < N; i++) v.push_back(rand()%10);
@@ -41,4 +23,17 @@ int dotProduct(vector<int> x,vector<int> y){
 	int sum = 0;
 	for(unsigned int i = 0; i<x.size(); i++) sum += x[i]*y[i];	
 	return sum;
+}
+
+int main(){
+	srand(time(0));
+	vector<int> x = randomVector(5);
+	vector<int> y = randomVector(5);
+	int z = dotProduct(x,y);
+	showVector(x);
+	cout << " . ";
+	showVector(y);
+	cout << " = " << z;
+	
+	return 0;
 }
